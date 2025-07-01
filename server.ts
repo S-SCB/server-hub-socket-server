dcf46b2e59c7093c52254b2560a0c0fa62e33e53
Saurabh Chandra Baij <saurabhbaij@gmail.com>
2025-07-01 15:37:00 +0530

    Updated the CORS origin link.

--- server.ts
+++ server.ts
@@ -15,7 +15,7 @@ interface DirectMessage {
 // Create HTTP server with a basic response so Render can detect an open port
 const httpServer = createServer((req, res) => {
   // Set CORS headers
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000/");
+  res.setHeader("Access-Control-Allow-Origin", "https://server-hub-optimised-ten.vercel.app");
   res.setHeader("Access-Control-Allow-Credentials", "true");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
@@ -34,7 +34,7 @@ const httpServer = createServer((req, res) => {
 
 const io = new Server(httpServer, {
   cors: {
-    origin: "http://localhost:3000/",
+    origin: "https://server-hub-optimised-ten.vercel.app",
     methods: ["GET", "POST"],
     credentials: true, // Ensure credentials are allowed
   },
